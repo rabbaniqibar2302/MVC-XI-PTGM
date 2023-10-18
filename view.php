8543bee1b0ff671d7cade255151f3ada33550e29
rabbaniqibar2302 <rizkiakbar2302@gmail.com>
2023-10-18 07:55:47 +0700

    fixin view

--- view.php
+++ view.php
@@ -1,18 +1,50 @@
 <html>
-    <head>
-        <title>Latihan MVC</title>
-        <style>
-            /* Global Style */
-            body{
-                background-color: #F8F8F8;
-            }
-            .container{
-                width: 960px;
-                padding: 10px 50px 50px;
-            }
-        </style>
-    </head>
-    <body>
+
+<head>
+    <title>Latihan MVC</title>
+    <style>
+        /* Global Style */
+        body {
+            background-color: #F8F8F8;
+        }
+
+        div.container {
+            width: 960px;
+            padding: 10px 50px 50px;
+            background-color: white;
+            margin: 20px auto;
+            box-shadow: 1px 0px 10px, -1px 0px 10px;
+        }
+
+        h1 {
+            text-align: center;
+            font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+            clear: both;
+        }
+
+        /* TABLE DESIGN */
+        table {
+            border-collapse: collapse;
+            border-spacing: 0;
+            border: 1px black solid;
+            width: 100%;
+        }
+
+        th,
+        td {
+            padding: 8px 15px;
+            border: 1px black solid;
+        }
+
+        tr:nth-child(2n+3) {
+            background-color: #F2F2F2;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Data User | Latihan MVC</h1>
         <table border="1">
             <tr>
                 <th>No</th>
@@ -20,6 +52,10 @@
                 <th>Email</th>
                 <th>Alamat</th>
             </tr>
+            <?php
+            ?>
         </table>
-    </body>
+    </div>
+</body>
+
 </html>
