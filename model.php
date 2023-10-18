57ae3096534e4f2953c675b4fa37341055b6c84d
rabbaniqibar2302 <rizkiakbar2302@gmail.com>
2023-10-18 07:09:49 +0700

    belajar mvc

---
+++ model.php
@@ -0,0 +1,15 @@
+<?php
+// Bagian MVC yang mengurusi database, semua hal yang terkait database, ditulis di Model
+function db(){
+    return mysqli_connect("localhost","root",null,"mvc_data");
+}
+
+function ambil_table(){
+    $link = db();
+    $table = mysqli_query($link,"select * from user");
+
+    // kita explode data dari db ke array aso
+    $result = mysqli_fetch_all($table, MYSQLI_ASSOC);
+    return $result;
+}
+?>
---
+++ view.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Latihan MVC</title>
+        <style>
+            /* Global Style */
+            body{
+                background-color: #F8F8F8;
+            }
+            .container{
+                width: 960px;
+                padding: 10px 50px 50px;
+            }
+        </style>
+    </head>
+    <body>
+        <table border="1">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Alamat</th>
+            </tr>
+        </table>
+    </body>
+</html>
